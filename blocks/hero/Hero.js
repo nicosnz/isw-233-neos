f0e5128cb2c7646180896c97be82e58b95f6fe79
Emanuel Oly <olysancheznicolas@gmail.com>
2026-03-13 11:04:10 -0400

    creacion de web components: footer,navbar,sobremi,hero

---
+++ blocks/hero/Hero.js
@@ -0,0 +1,31 @@
+class Hero extends HTMLElement {
+
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        this.innerHTML = `
+        <section class="hero">
+            <div class="hero__contenedor">
+            <div class="hero__contenido">
+                <h1 class="hero__titulo">Emanuel Oly</h1>
+                <p class="hero__objetivo">Mentalidad startup. Ejecución imparable.<span class="accent"></span></p>
+                <p class="hero__descripcion">Proactivo, estratégico y orientado a resultados.<br>Transformo problemas complejos en soluciones simples y escalables.</p>
+                <a href="#contact" class="btn btn--green">Trabajemos Juntos &rsaquo;</a>
+            </div>
+            <div class="hero__imagen">
+                <div class="hero__recortada">
+                <img
+                    src="img/perfil.jpeg"
+                    alt="Emanuel Oly"
+                />
+                </div>
+            </div>
+            </div>
+        </section>
+        `;
+    }
+}
+
+customElements.define("app-hero", Hero);
---
+++ index.js
@@ -0,0 +1,4 @@
+import "./blocks/navbar/Navbar.js";
+import "./blocks/hero/Hero.js";
+import "./blocks/sobre-mi/SobreMi.js";
+import "./blocks/footer/Footer.js";
